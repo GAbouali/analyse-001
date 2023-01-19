35c69548285cb5d396cab4ece6882cb6bd11f659
Your Gomaa <gomaamhmd@gmail.com>
2023-01-20 00:04:55 +0300

    003

--- Seminar_1--task3/Program.cs
+++ Seminar_1--task3/Program.cs
@@ -1,1 +1,26 @@
-﻿
+﻿// Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+
+Console.WriteLine("Enter day number: ");
+string? day = Console.ReadLine();
+
+//check that input isnt empty.
+if(day!=null)
+{
+    //make the program to treat with the input as a number.
+    int dayname = int.Parse(day);
+
+    //creatin an array.
+    string[] weekday = new string[7];
+    weekday[0] = "monday";
+    weekday[1] = "tuseday";
+    weekday[2] = "wednesday";
+    weekday[3] = "thursday";
+    weekday[4] = "friday";
+    weekday[5] = "saturday";
+    weekday[6] = "sunday";
+
+    Console.WriteLine(weekday[dayname-1]);
+
+}
+
+
