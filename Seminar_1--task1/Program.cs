38b4dfa0f11adcacb2fb3fcfc9976bd145372c4c
Your Gomaa <gomaamhmd@gmail.com>
2023-01-19 15:39:19 +0300

    -a

---
+++ Seminar_1--task1/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача №1
+// Напишите программу, которая на вход принимает
+// два числа и проверяет, является ли первое число
+// квадратом второго.
+
+Console.WriteLine("inter first number: ");
+string? input1 = Console.ReadLine();
+
+Console.WriteLine("inter first number: ");
+string? input2 = Console.ReadLine();
+
+if(input1!=null && input2!=null )
+{
+    int number1 = int.Parse(input1);
+    int number2 = int.Parse(input2);
+
+    if(number1 == number2*number2)
+    {
+        Console.WriteLine("first num is the square of first number, ");
+    }
+    else
+    {
+         Console.WriteLine("first num isnt the square of first number, ");
+
+    }
+}
+
