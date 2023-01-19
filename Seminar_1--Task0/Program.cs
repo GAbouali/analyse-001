9ecf4bb0aeeb505d032fe718b5deb72293082054
Your Gomaa <gomaamhmd@gmail.com>
2023-01-19 15:48:22 +0300

    -a

---
+++ Seminar_1--Task0/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача №0
+// Напишите программу, которая на вход принимает
+// число и выдаёт его квадрат (число умноженное на
+// само себя). 
+
+//request the required data.
+Console.WriteLine("input number, ");
+//read the intered data from console.
+string? inputnum = Console.ReadLine();
+//get sure that data isnt empty.
+if(inputnum!=null)
+{
+    //analyse and classify the interd number.
+    int number = int.Parse(inputnum);
+
+//get the square of this number.
+    int result = number*number;
+//output data on the console.
+Console.WriteLine(result);
+
+}
+
+
+
