84c20dfeae3ad3e7966fdce54b5db00bbf8e3b2c
Your Gomaa <gomaamhmd@gmail.com>
2023-01-20 15:07:35 +0300

    005

---
+++ Seminar_1--task5/Program.cs
@@ -0,0 +1,21 @@
+﻿//Напишите программу, которая на вход принимает одно число (N),
+// а на выходе показывает все целые числа в промежутке от -N до N.
+
+Console.WriteLine("enter number, ");
+string? num = Console.ReadLine();
+
+if(num!=null)
+{
+
+    int A = int.Parse(num);
+    int C = A*(-1);
+    string R = string.Empty;
+
+    while(C<A)
+    {
+        R = R+C+",";
+        C = C+1;
+    }
+    R = R+A;
+    Console.WriteLine(R);
+}
